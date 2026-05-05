93053565b1f9dfc331a1fe47b8e9c31fbe3a85cc
Kristian Elde Johansen <kristian.elde@proton.me>
2026-05-05 11:27:02 +0200

    personalized project name

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def main(args: argparse.Namespace):
 
     run = wandb.init(
         entity="equivariant-drifting",
-        project="tests",
+        project="tests-kristian",
         group=args.group_tag,
         mode="offline" if args.offline else "online",
         config=vars(args),
