6f72679afb56252df6a216664e4c7c0cd27bbe5f
slndaniel <Danielsleiman@outlook.com>
2026-05-08 16:55:28 +0200

    had wrong folder in train.sh

--- jobs/train.sh
+++ jobs/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --job-name=test_train
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=9
+#SBATCH --cpus-per-task=16
 #SBATCH --time=00:30:00
 #SBATCH --output=slurm_output_%A.out
 
@@ -13,6 +13,6 @@ module load 2025
 module load Anaconda3/2025.06-1
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate equi
-cd "$HOME/drifting-experiments"
+cd "$HOME/dutch-daniel2"
 
 python train.py --sample_frac 0.025 --lr 2e-5 --check_val_every_n_epoch 6 --max_epochs 20 --num_workers 8
