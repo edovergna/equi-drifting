d537057fbdb5f261c7662fbe89e4d8f976f18a1f
snellius <scur0099@snellius.surf.nl>
2026-05-21 00:20:20 +0200

    train file

--- jobs/train.sh
+++ jobs/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu_a100
+#SBATCH --partition=gpu_h100
 #SBATCH --gpus=1
-#SBATCH --job-name=test_train
+#SBATCH --job-name=aligned
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=04:00:00
+#SBATCH --time=01:00:00
 #SBATCH --output=slurm_output_%A.out
 
 module purge
@@ -17,13 +17,10 @@ conda activate equi
 cd "$HOME/drifting-experiments"
 
 python train.py \
-  --group_tag test_train \
-  --sample_frac 0.01 \
-  --n_real_molecules 4 \
-  --n_gen_molecules 32 \
-  --max_num_atoms 18 \
+  --n_real_molecules 128 \
+  --n_gen_molecules 128 \
   --num_workers 4 \
-  --max_epochs 10 \
+  --max_epochs 1 \
   --check_val_every_n_epoch 1 \
-  --hidden_dim 64 \
-  --num_layers 6
+  --hidden_dim 256 \
+  --num_layers 9
