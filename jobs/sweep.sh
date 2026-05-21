a467107bf0bb81853f93fc70acfaccdc6ceed10c
snellius <scur0099@snellius.surf.nl>
2026-05-21 11:50:39 +0200

    fix

--- jobs/sweep.sh
+++ jobs/sweep.sh
@@ -7,7 +7,8 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --time=04:00:00
 #SBATCH --output=slurm_output_%A.out
-
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=olivier.stam@student.uva.nl
 module purge
 module load 2025
 module load Anaconda3/2025.06-1
--- model/__init__.py
+++ model/__init__.py
@@ -6,16 +6,3 @@ from .datamodule import QM9DataModule
 from .lit_module import MoleculeGenerator
 from .train_utils import initialize_training_config
 
-try:
-    from .litmodules.euclidean import EuclideanGenerator
-except ModuleNotFoundError as exc:
-    if exc.name != "model.litmodules.euclidean":
-        raise
-    EuclideanGenerator = None
-
-try:
-    from .litmodules.riemannian import RiemannianGenerator
-except ModuleNotFoundError as exc:
-    if exc.name != "model.litmodules.riemannian":
-        raise
-    RiemannianGenerator = None
