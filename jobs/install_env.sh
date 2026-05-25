454fdd8a2f29accd50b2f3ce574175b4835e1784
Kristian Elde Johansen <kristian.elde@proton.me>
2026-05-25 19:55:05 +0200

    anonymize

--- jobs/install_env.sh
+++ jobs/install_env.sh
@@ -7,8 +7,6 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --time=00:10:00
 #SBATCH --output=slurm_output_%A.out
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=your.email@university.nl
 
 module purge
 module load 2025
