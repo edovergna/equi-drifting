572387b351c386ff838e43ba66bd4660d13f637e
edovergna <evergnano49@gmail.com>
2026-06-08 21:58:34 +0200

    update train job file

--- jobs/train.sh
+++ jobs/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu_h100
+#SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --job-name=aligned
+#SBATCH --job-name=types
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=slurm_output_%A.out
 
 module purge
@@ -16,22 +16,11 @@ conda activate equi
 
 cd "$HOME/drifting-experiments"
 
-python train.py \
+python train_types.py \
   --n_real_molecules 128 \
   --n_gen_molecules 128 \
   --num_workers 4 \
-  --max_epochs 500 \
+  --max_epochs 200 \
   --min_num_atoms 3 \
-  --max_num_atoms 3 \
-  --check_val_every_n_epoch 1 \
-  --hidden_dim 128 \
-  --num_layers 4 \
-  --max_iter 1 \
-  --sample_frac 1.0 \
-  --position_sigma 2.0 \
-  --types_sigma 1.0 \
-  --position_eta 1.0 \
-  --end_sigma 0.8 \
-  --types_eta 1.0 \
-  --lr 1e-5 \
-  --position_weight 0.5 \
+  --max_num_atoms 10 \
+  --lr 2e-5
